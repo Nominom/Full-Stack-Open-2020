11e4bc356b4ca65b432cd22475b75ff17fe87f7b
Nominom <nomi.lakkala@gmail.com>
2020-04-09 17:35:39 +0300

    1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,33 +2,48 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+	// save clicks of each button to own state
+	const [good, setGood] = useState(0)
+	const [neutral, setNeutral] = useState(0)
+	const [bad, setBad] = useState(0)
 
-  const increaseGood = () => setGood(good + 1)
-  const increaseNeutral = () => setNeutral(neutral + 1)
-  const increaseBad = () => setBad(bad + 1)
+	const increaseGood = () => setGood(good + 1)
+	const increaseNeutral = () => setNeutral(neutral + 1)
+	const increaseBad = () => setBad(bad + 1)
 
-  return (
-    <div>
-      <h1>give feedback</h1>
-	  <div>
-		  <button onClick={increaseGood}>good</button>
-		  <button onClick={increaseNeutral}>neutral</button>
-		  <button onClick={increaseBad}>bad</button>
-	  </div>
-	  <h1>statistics</h1>
-	  <div>
-		  <div>good {good}</div>
-		  <div>neutral {neutral}</div>
-		  <div>bad {bad}</div>
-	  </div>
-    </div>
-  )
+	const all = () => {
+		return good + neutral + bad;
+	}
+
+	const average = () => {
+		return (good * 1 + neutral * 0 + bad * -1) / all();
+	}
+
+	const positive = () => {
+		return good / all() * 100;
+	}
+
+	return (
+		<div>
+			<h1>give feedback</h1>
+			<div>
+				<button onClick={increaseGood}>good</button>
+				<button onClick={increaseNeutral}>neutral</button>
+				<button onClick={increaseBad}>bad</button>
+			</div>
+			<h1>statistics</h1>
+			<div>
+				<div>good {good}</div>
+				<div>neutral {neutral}</div>
+				<div>bad {bad}</div>
+				<div>all {all()}</div>
+				<div>average {average()}</div>
+				<div>positive {positive()}%</div>
+			</div>
+		</div>
+	)
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
+ReactDOM.render(<App />,
+	document.getElementById('root')
 )
