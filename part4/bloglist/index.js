100f4cf966efca3684d29ae84c684380806fb6db
Nominom <nomi.lakkala@gmail.com>
2020-04-28 16:16:52 +0300

    4.3 ESLint

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,24 +1,24 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-const blogsRouter = require('./controllers/blogs')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const blogsRouter = require('./controllers/blogs');
 
-const mongoUrl = config.MONGODB_URI
+const app = express();
+
+const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then(() => {logger.info('connected to MongoDB')})
-	.catch((error) => {
-		logger.error('Error connecting to MongoDB: ', error.message)
-	})
+  .then(() => { logger.info('connected to MongoDB'); })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB: ', error.message);
+  });
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
+app.use(cors());
+app.use(express.json());
+app.use('/api/blogs', blogsRouter);
 
-const PORT = config.PORT
+const { PORT } = config;
 app.listen(PORT, () => {
-	logger.info(`Server running on port ${PORT}`)
-})
+  logger.info(`Server running on port ${PORT}`);
+});
