a265d886440541f71c93742f847a8c02fdc2f6b3
Nominom <nomi.lakkala@gmail.com>
2020-04-30 17:10:27 +0300

    4.10

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -20,6 +20,28 @@ test('correct amount of blogs are returned', async () => {
 	expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('a new blog is created', async () => {
+	const newBlog = {
+		title: "Blog4",
+		author: "Authori",
+		url: "localhost",
+		likes: 55,
+	}
+	await api
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(201)
+		.expect('Content-Type', /application\/json/)
+
+	const response = await api.get('/api/blogs')
+	expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+	
+	expect(response.body.map(r => r.title)).toContain(newBlog.title);
+	expect(response.body.map(r => r.author)).toContain(newBlog.author);
+	expect(response.body.map(r => r.url)).toContain(newBlog.url);
+	expect(response.body.map(r => r.likes)).toContain(newBlog.likes);
+})
+
 afterAll(() => {
 	mongoose.connection.close()
 })
